4882bf93a8f362f5033596e9dafb11dcf18bad77
MircoFernando <mircofdo@gmail.com>
2026-06-01 18:15:53 +0530

    run using nodemon

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 # Import Base Image
-FROM node:20-alphine
+FROM node:20-alpine
 
 # Set Working Directory
 WORKDIR /app
 
+# Copy package.json and package-lock.json
 COPY . .
+
+CMD ["node", "index.js"]
 
 
--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const express = require('express');
+let name = "Mirco Fernando";
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+console.log(`Hello, ${name}! Welcome to Docker Learn.`);
